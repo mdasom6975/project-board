44763971f47866fc8e569f032fa4bdcdd20b6766
dsmoon <mdasom6975@gmail.com>
2023-02-13 10:59:05 +0900

    * DB 접근을 위한 repository interface 작성

---
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,10 @@
+package com.fastcampus.projectboard.repository;
+
+import com.fastcampus.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * JpaRepository 의 구현체인 SimpleJpaRepository 에 @Repository 이미 가지고 있어 어노테이션 사용하지 않음
+ */
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
