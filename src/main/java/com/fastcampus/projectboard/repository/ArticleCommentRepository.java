b84eb109b0cd83c305ad75b7fa81fa7a9e490ccd
문다솜 <mdasom6975@gmail.com>
2023-03-02 11:25:46 +0900

    * data rest 설정

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -2,9 +2,11 @@ package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * JpaRepository 의 구현체인 SimpleJpaRepository 에 @Repository 이미 가지고 있어 어노테이션 사용하지 않음
  */
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
